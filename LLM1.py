c7ae1c6279473b2901ea62d7ed289c1a4edc6174
Ritesh Ranjan <ritsrnjn@Riteshs-MacBook-Pro.local>
2023-06-10 14:51:35 +0530

    added gitgnore

--- LLM1.py
+++ LLM1.py
@@ -4,26 +4,14 @@ import os
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
-# write a function that takes in a query and returns an answer
-
-def function(query):
-    return "This is a test answer"
-
-
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import GoogleDriveLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-# import openai
 
-# openai.openai_api_key='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'
-
-
-
-
-folder_id = "1_tRD6uhQnv7V72m6L8XmDxSWJHtUNmIg"
+folder_id = "1jRxpAwHVEHzYoMAlsM-sTITGRmRLB7U1"
 loader = GoogleDriveLoader(
     folder_id=folder_id,
     recursive=False
@@ -31,7 +19,7 @@ loader = GoogleDriveLoader(
 docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=4000, chunk_overlap=0, separators=[" ", ",", "\n"]
-    )
+)
 
 texts = text_splitter.split_documents(docs)
 embeddings = OpenAIEmbeddings()
@@ -41,12 +29,8 @@ retriever = db.as_retriever()
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 
-# 
-
 
 while True:
     query = input("> ")
     answer = qa.run(query)
     print(answer)
-
-   
